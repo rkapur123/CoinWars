80ec36a32265de6223b8015d8e18bf9f2342be4e
RAHUL KAPUR <rk2749@columbia.edu>
2018-04-07 22:19:47 -0700

    updated token prcision

--- src/server/erc20_rinkeby.js
+++ src/server/erc20_rinkeby.js
@@ -27,19 +27,19 @@ const server = {
       this.address = addrArray[0]
 
       // deploy first token
-      const token1Contract = await this.deployDummyToken(100000000000, 'Tronix', 6, 'TRX')
+      const token1Contract = await this.deployDummyToken(100000000000000000, 'Tronix', 6, 'TRX')
       console.log(`First token TRX has been deployed at ${token1Contract.address}`)
 
       // deploy second token
-      const token2Contract = await this.deployDummyToken(	1000000000, 'EOS', 18, 'EOS')
+      const token2Contract = await this.deployDummyToken(	1000000000000000000000000000, 'EOS', 18, 'EOS')
       console.log(`Second token EOS has been deployed at ${token2Contract.address}`)
 
       // deploy third token
-      const token3Contract = await this.deployDummyToken(197192382, 'Binance Coin', 18, 'BNB')
+      const token3Contract = await this.deployDummyToken(197192382000000000000000000, 'Binance Coin', 18, 'BNB')
       console.log(`Third token BNB has been deployed at ${token3Contract.address}`)
 
       // deploy fourth token
-      const token4Contract = await this.deployDummyToken(1000000000, 'VeChain', 18, 'VEN')
+      const token4Contract = await this.deployDummyToken(1000000000000000000000000000, 'VeChain', 18, 'VEN')
       console.log(`Fourth token VEN has been deployed at ${token4Contract.address}`)
 
       let balance1 = await token1Contract.balanceOf(this.address)
@@ -58,10 +58,10 @@ const server = {
 
       // create dummy balances TRX
       // 0x77bc416c65fc676f6dc598cda2797496280b2170 - 1800
-      await token1Contract.transfer(addrArray[1], 50000000000, { from: `${this.address}`, gas: 5000000 })
+      await token1Contract.transfer(addrArray[1], 50000000000000000, { from: `${this.address}`, gas: 5000000 })
       console.log(`50000000000 TRX successfully transferred to ` + addrArray[1])
 
-      await token1Contract.transfer('0x9fa877509a7b5d90bdbdf911aeeaacce12857521', 50000000000, { from: `${this.address}`, gas: 5000000 })
+      await token1Contract.transfer('0x9fa877509a7b5d90bdbdf911aeeaacce12857521', 50000000000000000, { from: `${this.address}`, gas: 5000000 })
       console.log(`50000000000 TRX successfully transferred to 0x9fa877509a7b5d90bdbdf911aeeaacce12857521`)
 
       // 0x37c69511c5ba54798accf6a6be6ac87de31a09b1 - 2500
@@ -76,10 +76,10 @@ const server = {
 
       // create dummy balances EOS
       // 0x77bc416c65fc676f6dc598cda2797496280b2170 - 2000
-      await token2Contract.transfer(addrArray[1], 500000000, { from: `${this.address}`, gas: 5000000 })
+      await token2Contract.transfer(addrArray[1], 500000000000000000000000000, { from: `${this.address}`, gas: 5000000 })
       console.log(`500000000 EOS successfully transferred to ` + addrArray[1])
 
-      await token2Contract.transfer('0x9fa877509a7b5d90bdbdf911aeeaacce12857521', 500000000, { from: `${this.address}`, gas: 5000000 })
+      await token2Contract.transfer('0x9fa877509a7b5d90bdbdf911aeeaacce12857521', 500000000000000000000000000, { from: `${this.address}`, gas: 5000000 })
       console.log(`500000000 EOS successfully transferred to 0x9fa877509a7b5d90bdbdf911aeeaacce12857521`)
       // 0x37c69511c5ba54798accf6a6be6ac87de31a09b1 - 3200
       //await token2Contract.transfer(addrArray[2], 3200, { from: `${this.address}`, gas: 5000000 })
@@ -92,10 +92,10 @@ const server = {
 
       // create dummy balances BNB
       // 0x77bc416c65fc676f6dc598cda2797496280b2170 - 2500
-      await token3Contract.transfer(addrArray[1], 98596191, { from: `${this.address}`, gas: 5000000 })
+      await token3Contract.transfer(addrArray[1], 98596191000000000000000000, { from: `${this.address}`, gas: 5000000 })
       console.log(`98596191 BNB successfully transferred to ` + addrArray[1])
 
-      await token3Contract.transfer('0x9fa877509a7b5d90bdbdf911aeeaacce12857521', 98596191, { from: `${this.address}`, gas: 5000000 })
+      await token3Contract.transfer('0x9fa877509a7b5d90bdbdf911aeeaacce12857521', 98596191000000000000000000, { from: `${this.address}`, gas: 5000000 })
       console.log(`98596191 BNB successfully transferred to 0x9fa877509a7b5d90bdbdf911aeeaacce12857521`)
       // 0x37c69511c5ba54798accf6a6be6ac87de31a09b1 - 1200
       //await token3Contract.transfer(addrArray[2], 1200, { from: `${this.address}`, gas: 5000000 })
@@ -108,10 +108,10 @@ const server = {
 
       // create dummy balances VEN
       // 0x77bc416c65fc676f6dc598cda2797496280b2170 - 1400
-      await token4Contract.transfer(addrArray[1], 500000000, { from: `${this.address}`, gas: 5000000 })
+      await token4Contract.transfer(addrArray[1], 500000000000000000000000000, { from: `${this.address}`, gas: 5000000 })
       console.log(`500000000 VEN successfully transferred to ` + addrArray[1])
 
-      await token4Contract.transfer('0x9fa877509a7b5d90bdbdf911aeeaacce12857521', 500000000, { from: `${this.address}`, gas: 5000000 })
+      await token4Contract.transfer('0x9fa877509a7b5d90bdbdf911aeeaacce12857521', 500000000000000000000000000, { from: `${this.address}`, gas: 5000000 })
       console.log(`500000000 VEN successfully transferred to 0x9fa877509a7b5d90bdbdf911aeeaacce12857521`)
       // 0x37c69511c5ba54798accf6a6be6ac87de31a09b1 - 1600
       //await token4Contract.transfer(addrArray[2], 1600, { from: `${this.address}`, gas: 5000000 })
